1e6dc274a457ed0c2b63788d662c54a6fcb12db3
Alok Kumar Mahato <alokrm2008@gmail.com>
2026-06-18 20:01:57 +0530

    Add references for buttons and modals
    Initialize button and modal element references.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Get buttons and modal elements - Alok
+const addBtn = document.getElementById("addBtn");
+const filterBtn = document.getElementById("filterBtn");
 
+const taskModal = document.getElementById("taskModal");
+const filterModal = document.getElementById("filterModal");
+
+const taskInput = document.getElementById("taskInput");
+const searchInput = document.getElementById("searchInput");
